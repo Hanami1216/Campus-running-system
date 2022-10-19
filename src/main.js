18a8cfbf207bdd282f5f82ce7bfbdd7f962a172f
YOKI1216 <1151509140@qq.com>
2022-10-19 14:05:44 +0800

    a

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+import App from './App.vue'
+
+
+const app = createApp(App)
+app.use(router)
+
+
+app.use(ElementPlus)
+
+app.mount('#app')
+
+app.use(ElementPlus, {
+    locale: zhCn,
+  })
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import about from '../views/AboutView.vue'
+import login from '../views/login/Login.vue'
 
 const routes = [
   {
@@ -13,9 +15,12 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
+    component: about
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: login
   }
 ]
 
