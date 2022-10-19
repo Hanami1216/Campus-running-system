73bfbac95f745383a960b41e332056458b0fe54a
YOKI1216 <1151509140@qq.com>
2022-10-19 16:16:19 +0800

    login

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import about from '../views/AboutView.vue'
+
 import login from '../views/login/Login.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: about
-  },
-  {
-    path: '/login',
     name: 'login',
     component: login
   }
