e8219fb1f1c564407bb9f7dece5d26895e51dd77
YOKI1216 <1151509140@qq.com>
2023-02-24 16:45:45 +0800

    表格导入

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,55 +1,61 @@
 <template>
     <el-container class="layout-container-demo" style="height: 100%">
       <!-- 侧边栏 -->
-      <el-aside width="200px">
-
-        <el-scrollbar>
-          <!-- 菜单 -->
-          <el-menu>
-            
-            <!-- 菜单  -->
-            <el-sub-menu index="1">
-              <!--菜单标题 -->
-              <template #title>
-                <el-icon><setting /></el-icon>用户
-              </template>
-              <!-- 子菜单 -->
-              <el-menu-item-group>
-                <template #title>查询功能</template>
-                <el-menu-item index="1-1">查询所有</el-menu-item>
-                <el-menu-item index="1-2">查询</el-menu-item>
-              </el-menu-item-group>
-            </el-sub-menu>
-
-          </el-menu>
-        </el-scrollbar>
-      </el-aside>
 
       <!-- 页面 -->
       <el-container>
         <el-header>
-            
+            <h1 >校园跑腿系统</h1>
         </el-header>
         <!-- 数据 -->
         <el-main>
           <el-scrollbar>
-            <el-button>123</el-button>
+            <el-table :data="oder" border style="width: 100%">
+              <el-table-column prop="id" label="id" width="180" />
+              <el-table-column prop="content" label="content" width="180" />
+              <el-table-column prop="payment_method" label="payment_method" />
+              <el-table-column prop="telephone_number" label="telephone_number" width="180" />
+              <el-table-column prop="update_time" label="update_time" width="180" />
+            </el-table>
           </el-scrollbar>
         </el-main>
+        <el-footer>
+            
+        </el-footer>
       </el-container>
       
     </el-container>
   </template>
-  
 
+  <script>
+  export default {
+    data(){
+        return{
+          // oder:{
+          //   "content": "订单发布内容",
+          //   "id": 1,
+          //   "payment_method": 1,
+          //   "telephone_number": "18038992335",
+          //   "uid": 1,
+          //   "update_time": null
+          // }
+        }
+    },
+    methods:{
 
-
-
+    }
+}
+</script>
 
   
   <style scoped>
   .layout-container-demo .el-header {
     position: relative;
+    background-color: var(--el-color-primary-light-7);
+    color: var(--el-text-color-primary);
+  }
+  .layout-container-demo .el-footer {
+    position: absolute; bottom: 1;
     background-color: var(--el-color-primary-light-7);
     color: var(--el-text-color-primary);
   }
